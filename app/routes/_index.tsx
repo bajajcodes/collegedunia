67d4530b176101f2bce089e46da9df3ee40e0dd4
shubham bajaj <shmbajaj32@gmail.com>
2024-02-02 02:00:22 +0530

    fix: social links and page title color

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -54,8 +54,10 @@ export default function Index() {
     <div className="container relative mb-8">
       <section className="relative h-[580px]">
         <PageHeader className="pb-8 absolute top-16 left-1 z-40 w-full max-w-lg ">
-          <p className="capitalize">TAKE YOUR FUTURE TO NEW HEIGHTS</p>
-          <PageHeaderHeading className="capitalize">
+          <p className="capitalize text-white">
+            TAKE YOUR FUTURE TO NEW HEIGHTS
+          </p>
+          <PageHeaderHeading className="capitalize text-white font-semibold">
             Explore expert education consultants for personalized guidance
           </PageHeaderHeading>
           <div className="w-full flex items-center space-x-4 pb-8 pt-4 md:pb-10">
