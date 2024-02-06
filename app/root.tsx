39962bae280c5754bef6a57450360cfc152986cc
shubham bajaj <shmbajaj32@gmail.com>
2024-02-07 01:09:19 +0530

    feat: added analytics

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction } from '@remix-run/node';
+import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -6,15 +6,15 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from '@remix-run/react';
+} from "@remix-run/react";
 
-import styles from './tailwind.css';
-import { SiteHeader } from './components/site-header';
-import { SiteFooter } from './components/site-footer';
-import { Toaster } from './components/ui/toaster';
-import { WhatsAppButton } from './components/whatsapp-button';
+import { SiteFooter } from "./components/site-footer";
+import { SiteHeader } from "./components/site-header";
+import { Toaster } from "./components/ui/toaster";
+import { WhatsAppButton } from "./components/whatsapp-button";
+import styles from "./tailwind.css";
 
-export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }];
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export default function App() {
   return (
@@ -22,11 +22,40 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="google-site-verification"
+          content="2Jk2ErCmHHMF4o4GKenI16Wcu9gxxEvYB98MVpL9gsE"
+        />
+
         <link rel="preconnect" href="https://maps.google.com" />
         <link rel="preconnect" href="https://maps.gstatic.com" />
         <link rel="preconnect" href="https://maps.googleapis.com" />
         <Meta />
         <Links />
+        <script
+          async
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MBBHQ385');`,
+          }}
+        ></script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-WFXNR8EX3K"
+        ></script>
+        <script
+          async
+          dangerouslySetInnerHTML={{
+            __html: `  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WFXNR8EX3K');`,
+          }}
+        ></script>
       </head>
       <body className="relative min-h-screen bg-background flex flex-col font-sans">
         <SiteHeader />
@@ -40,6 +69,15 @@ export default function App() {
         <Scripts />
         <LiveReload />
       </body>
+      <noscript>
+        <iframe
+          title="no-script-analytics"
+          src="https://www.googletagmanager.com/ns.html?id=GTM-MBBHQ385"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
     </html>
   );
 }
